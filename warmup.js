e977693d0510ff24b889c00f82acf5c3e933bd71
Michael Palaiologos <Palaiologos1990@gmail.com>
2015-11-02 22:43:02 -0800

    Finish vowel_count function

--- warmup.js
+++ warmup.js
@@ -3,8 +3,27 @@
 // My Max function. Takes array and 
 // returns largest value.
 
-var my_max = function (array) {
+function my_max(array) {
 	return Math.max.apply(array);
 };
 
 console.log([3,94,25,85,10,1000,86,2,1]);
+
+// Vowel Count function. 
+
+function vowel_count(string1)
+{
+	var vowel_list = 'aeiouAEIOU';
+	var v_count = 0;
+
+	for (var i = 0; i < string1.length; i++)
+	{
+		if (vowel_list.indexOf(string1[i]) !== -1)
+		{
+			v_count +=1
+		};
+	};
+	return v_count;
+};
+
+alert(vowel_count("The quick brown fox"));
